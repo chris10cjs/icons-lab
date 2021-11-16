7f536edde131c83228525acf79c98132d196b0d4
Christian Silvero <63545412+chris10cjs@users.noreply.github.com>
2021-11-16 14:59:15 -0300

    init files

---
+++ src/test/java/com/chris10cjs/icons/icons/IconsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chris10cjs.icons.icons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IconsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
